33863c544928ec455a6db59dedcc70435e466c20
NKaty <nd.katy@gmail.com>
2019-04-21 22:00:59 +0300

    Add styles for not found page

--- src/components/notFound/NotFound.js
+++ src/components/notFound/NotFound.js
@@ -1,13 +1,46 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const StyledNotFound = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  h3 {
+    font-size: 5rem;
+    margin: 2rem 0;
+  }
+
+  p {
+    font-size: 1.5rem;
+    margin: 1rem 0;
+  }
+
+  @media (min-width: 400px) {
+    p {
+      font-size: 2rem;
+    }
+  }
+
+  @media (min-width: 600px) {
+    h3 {
+      font-size: 7rem;
+      margin: 4rem 0;
+    }
+
+    p {
+      font-size: 3rem;
+      margin: 2rem 0;
+    }
+  }
+`
 
 const NotFound = () => {
   return (
-    <div>
-      <div>
-        <p>404</p>
-        <p>Page not found</p>
-      </div>
-    </div>
+    <StyledNotFound>
+      <h3>404</h3>
+      <p>Страница не найдена</p>
+    </StyledNotFound>
   )
 }
 
